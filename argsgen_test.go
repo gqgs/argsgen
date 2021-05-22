6935467add492ded760d023ffb979c0b3a284503
Guilherme <gqgs@protonmail.com>
2021-05-22 01:25:43 -0300

    test: create common compare func

--- argsgen_test.go
+++ argsgen_test.go
@@ -7,13 +7,13 @@ import (
 	"testing"
 )
 
-func Test_parse_options(t *testing.T) {
+func compare(t *testing.T, inputFile, outputFile string) {
 	builder := new(strings.Builder)
-	err := parse("testdata/options.go", "options", builder)
+	err := parse(inputFile, "options", builder)
 	if err != nil {
 		t.Fatal(err)
 	}
-	expected, err := ioutil.ReadFile("testdata/options_gen.go")
+	expected, err := ioutil.ReadFile(outputFile)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -33,28 +33,10 @@ func Test_parse_options(t *testing.T) {
 	}
 }
 
+func Test_parse_options(t *testing.T) {
+	compare(t, "testdata/options.go", "testdata/options_gen.go")
+}
+
 func Test_parse_required(t *testing.T) {
-	builder := new(strings.Builder)
-	err := parse("testdata/required.go", "options", builder)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expected, err := ioutil.ReadFile("testdata/required_gen.go")
-	if err != nil {
-		t.Fatal(err)
-	}
-	spaceRemover := regexp.MustCompile("\\s+")
-	got := spaceRemover.ReplaceAllString(builder.String(), "\n")
-	want := spaceRemover.ReplaceAllString(string(expected), "\n")
-
-	if cmp := strings.Compare(want, got); cmp != 0 {
-		want := strings.Split(want, "\n")
-		got := strings.Split(got, "\n")
-
-		for i := 0; i < len(want) && i < len(got); i++ {
-			if strings.Compare(want[i], got[i]) != 0 {
-				t.Fatalf("\nparse.%d():\n\twant:%q\n\tgot: %q", i, want[i], got[i])
-			}
-		}
-	}
+	compare(t, "testdata/required.go", "testdata/required_gen.go")
 }
